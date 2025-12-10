56ae5329e2ac172e8d0f6a4d3c8146fa393ae905
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2025-12-10 12:17:20 +0100

    Inicialización del proyecto de backend.

---
+++ backend/src/main/java/com/miniblog/backend_miniblog/BackendMiniblogApplication.java
@@ -0,0 +1,13 @@
+package com.miniblog.backend_miniblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiniblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiniblogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/miniblog/backend_miniblog/BackendMiniblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniblog.backend_miniblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiniblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
